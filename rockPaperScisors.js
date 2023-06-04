c9217c73bf3a6dae911cf921f052b691c81708ad
laisjmvet <laisjmvet@gmail.com>
2023-06-04 18:56:36 +0100

    Writting comments

--- rockPaperScisors.js
+++ rockPaperScisors.js
@@ -9,6 +9,7 @@
 import prompt from "prompt";
 import colors from "@colors/colors"
 
+//Set the prompt patterns and msgs when asking to play again
 const inputAskPlayAGain = {
     properties: {
         input: {
@@ -19,6 +20,7 @@ const inputAskPlayAGain = {
     }
 }
 
+//Set the prompt patterns and msgs when asking the player to choose rock, paper, or scissors
 const inputPlayGame = {
     properties: {
         input: {
@@ -35,7 +37,7 @@ const results = {
     "Loss": 0
 }
 
-//Check if the user wants to play again or not
+//Check if the player wants to play again or not
 const askPlayAgain = () => {
     //console.log(`Do you want to play again? Y/N`);    
     
@@ -55,7 +57,7 @@ const computerOutput = (arr = ["R", "P", "S"]) => {
     return arr[Math.floor(Math.random() *3)];
 };
 
-//Tells who is the winner
+//Tells who is the winner and update the results
 const winner = (input, computerOutput) => {
     let formatInput = input.toUpperCase().trim();
     console.log(colors.bgBrightBlue("\n" + formatInput + " X " + computerOutput));
@@ -90,13 +92,17 @@ prompt.start();
 const playGame = () => {    
     prompt.get(inputPlayGame, function (err, result) {
     
-    const output = computerOutput();
-    
-    winner(result.input, output);    
+    //assign the computer choice    
+    const output = computerOutput(); 
+    //checks who is the winner   
+    winner(result.input, output);  
+    //print the  results
     printResults();
+    //asks if the player wants to play again
     askPlayAgain();
     
   
 })};
 
+//Calling the function to play the game
 playGame();
